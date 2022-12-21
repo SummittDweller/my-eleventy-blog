5003bb8a3b51f897776d9841c11e67185ce370fa
Mark McFate <summitt.dweller@gmail.com>
2022-12-21 14:21:46 -0600

    Adding readableDT filter

--- .eleventy.js
+++ .eleventy.js
@@ -14,4 +14,11 @@ module.exports = function (eleventyConfig) {
     );
   });
 
+  // readableDT filter 
+  eleventyConfig.addFilter('readableDT', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'local' }).toFormat(
+      'yyyy-LLL-dd:HH.mm'
+    );
+  });
+  
 };
